e16eb758148a4398fd8196905dfff6cb3306263c
Damien Hampton <damien.hampton@gmail.com>
2019-12-31 18:22:30 +0000

    Added test endpoint Added catch-all 400 rule

--- src/app/my-app.js
+++ src/app/my-app.js
@@ -2,13 +2,20 @@
 
 function init({ app, appDb, logger = (...args) => console.log(...args) }){
 
-  app.use(async (req, res) => {
+  app.use(async (req, _, next) => {
     const log = { url: req.url, date: (new Date).toISOString()};
     await appDb.insertLog(log);
     logger(log);
+    next();
+  });
 
+  app.get('/log-test', async (_, res) => {
     const logs = await appDb.getLogs();
-    res.json(logs);
+    res.send(logs);
+  });
+
+  app.get('*', (_, res) => {
+    res.status(400).json({ message: 'bad request' });
   });
 
   return app;
